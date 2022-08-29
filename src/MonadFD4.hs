6deaa34b337fcbfcac4c25798715fe37c43798ab
Mauro Jaskelioff <mauro@fceia.unr.edu.ar>
2022-08-29 11:04:13 -0300

    Comentarios de MonadFD4

--- src/MonadFD4.hs
+++ src/MonadFD4.hs
@@ -47,21 +47,22 @@ import Control.Monad.Except
 import Control.Monad.Reader
 import System.IO
 
--- * La clase 'MonadFD4m'
+-- * La clase 'MonadFD4'
 
-{-| La clase de mónadas 'MonadFD4' clasifica a las mónadas con soporte para operaciones @IO@, estado de tipo 'Global.GlEnv', y errores de tipo 'Errors.Error'.
+{-| La clase de mónadas 'MonadFD4' clasifica a las mónadas con soporte para una configuración Global 'Global.Conf', 
+    para operaciones @IO@, estado de tipo 'Global.GlEnv', y errores de tipo 'Errors.Error'.
 
 Las mónadas @m@ de esta clase cuentan con las operaciones:
+   - @ask :: m Conf@
    - @get :: m GlEnv@
    - @put :: GlEnv -> m ()@
    - @throwError :: Error -> m a@
    - @catchError :: m a -> (Error -> m a) -> m a@
    - @liftIO :: IO a -> m a@
-   - @getConf :: m Conf
 
 y otras operaciones derivadas de ellas, como por ejemplo
-   - @modify :: (GlEnv -> GlEnv) -> m ()
-
+   - @modify :: (GlEnv -> GlEnv) -> m ()@
+   - @gets :: (GlEnv -> a) -> m a@  
 -}
 class (MonadIO m, MonadState GlEnv m, MonadError Error m, MonadReader Conf m) => MonadFD4 m where
 
@@ -124,7 +125,7 @@ catchErrors c = catchError (Just <$> c)
 ----
 -- Importante, no eta-expandir porque GHC no hace una
 -- eta-contracción de sinónimos de tipos
--- y Main no va a compilar al escribir `InputT FD4()`
+-- y Main no va a compilar al escribir `InputT FD4 ()`
 
 -- | El tipo @FD4@ es un sinónimo de tipo para una mónada construida usando dos transformadores de mónada sobre la mónada @IO@.
 -- El transformador de mónad @ExcepT Error@ agrega a la mónada IO la posibilidad de manejar errores de tipo 'Errors.Error'.
